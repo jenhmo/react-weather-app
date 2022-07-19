474e632215d3f84e0799081b6188cae2eef13488
jenhmo <101249416+jenhmo@users.noreply.github.com>
2022-07-18 17:42:20 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className ="container">
      Weather App
      <footer>
        This project was coded by Jen Morgan and is{" "}
-     <a href="https://github.com/jenhmo/react-weather-app" target="_blank">
+     <a href="https://github.com/jenhmo/react-weather-app" target="_blank" rel="noreferrer">
       open-sourced
      </a>
      </footer>
+    </div>
     </div>
   );
 }
