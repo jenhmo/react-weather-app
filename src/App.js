9ccd02ed40132bf55b17fc9b76ec04f25e5e33c2
jenhmo <101249416+jenhmo@users.noreply.github.com>
2022-07-18 17:52:51 -0700

    Added basic layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
+
 
 export default function App() {
   return (
     <div className="App">
       <div className ="container">
      Weather App
+     <Weather />
      <footer>
        This project was coded by Jen Morgan and is{" "}
      <a href="https://github.com/jenhmo/react-weather-app" target="_blank" rel="noreferrer">
