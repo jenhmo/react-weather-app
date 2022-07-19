261326d6f32c40bcae44548c313cab60dee84446
jenhmo <101249416+jenhmo@users.noreply.github.com>
2022-07-18 17:28:53 -0700

    Added github repo url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ export default function App() {
   return (
     <div className="App">
      Weather App
+     <footer>
+       This project was coded by Jen Morgan and is{" "}
+     <a href="https://github.com/jenhmo/react-weather-app" target="_blank">
+      open-sourced
+     </a>
+     </footer>
     </div>
   );
 }
