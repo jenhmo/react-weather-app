1bad83359d5325b47197cca3db261ebf67fa2c2b
jenhmo <101249416+jenhmo@users.noreply.github.com>
2022-07-20 14:17:19 -0700

    added API integration and css

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import React from "react";
 import Weather from "./Weather";
 import './App.css';
 
-
 export default function App() {
   return (
     <div className="App">
-      <div className ="container">
-     Weather App
-     <Weather />
-     <footer>
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
        This project was coded by Jen Morgan and is{" "}
      <a href="https://github.com/jenhmo/react-weather-app" target="_blank" rel="noreferrer">
       open-sourced
      </a>
      </footer>
-    </div>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,37 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather(){
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "97f38f62593364ddd3020cbfaa097437";
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiURL).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
     return (
-        <div className="Weather">
-            <form>
-                <div className="row">
-                <div className="col-9">
-                <input type="search" placeholder="Enter a city" className="form-control" />
-                </div>
-                <div className="col-3">
-                <input type="submit" value="Search" className="btn btn-primary" />
-                </div>
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
             </div>
-            </form>
-            <h1>Vienna</h1>
-            <ul>
-                <li>Wednesday 07:00</li>
-                <li>Mostly Cloudy</li>
-            </ul>
-            <div className="row">
-                <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Mostly Cloudy" />
-                    6˚C
-                </div>
-                <div className="col-6">
-                    <ul>
-                        <li>Precipitation: 15%</li>
-                        <li>Humidity: 72%</li>
-                        <li>Wind:13km/h</li>
-                    </ul>
-                </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
-            </div>
-    )
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
