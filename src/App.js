f0122a41e5bdf6b2c4b00aec4892a54975bdad1f
jenhmo <101249416+jenhmo@users.noreply.github.com>
2022-07-28 12:47:16 -0700

    added forecast

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Vienna" />
 
         <footer>
        This project was coded by Jen Morgan and is{" "}
